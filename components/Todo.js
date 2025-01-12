1f1f79a5af09ed4aad9fca933c4b1133ff472a2e
Tayo <omotayo_dnl@yahoo.com>
2025-01-11 23:17:07 -0600

    "edited Todo.js"

--- components/Todo.js
+++ components/Todo.js
@@ -1,23 +1,23 @@
 class Todo {
   constructor(data, selector) {
-    console.log(data);
-    console.log(selector);
     this._data = data;
     this._templateElement = document.querySelector("selector");
   }
 
   getView() {
-    const todoElement = this._templateElement.content
+    this._todoElement = this._templateElement.content
       .querySelector(".todo")
       .cloneNode(true);
 
-    const todoNameEl = todoElement.querySelector(".todo__name");
-    const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-    const todoLabel = todoElement.querySelector(".todo__label");
-    const todoDate = todoElement.querySelector(".todo__date");
-    const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
-    // console.log(`Name: ${this.name}`);
-    // console.log(`Name: ${this.name}`);
+    const todoNameEl = this._todoElement.querySelector(".todo__name");
+    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    const todoLabel = this._todoElement.querySelector(".todo__label");
+    const todoDate = this._todoElement.querySelector(".todo__date");
+    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+
+    return todoElement;
   }
 }
 
